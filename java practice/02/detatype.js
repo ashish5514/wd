08945448ae7b261db44c8f73aba20fa97d5c13dc
ashish5514 <zankatashish680@gmail.com>
2023-08-11 17:42:44 +0530

    work

---
+++ java practice/02/detatype.js
@@ -0,0 +1,29 @@
+//premetive
+
+//null
+let b= null;
+console.log(typeof b,b);
+
+//number
+let a = 100;
+console.log(typeof a,a);
+
+//boolean
+let c= true;
+console.log(typeof c,c);
+
+//bigint
+let d= BigInt(50)+BigInt(20);
+console.log(typeof d,d);
+
+//string
+let e = "name"
+console.log(typeof e,e);
+
+//symbole  
+let f = Symbol()
+console.log(typeof f,f);
+
+//undefined
+let g = undefined
+console.log(typeof g,g);
