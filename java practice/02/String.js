750e48cb6e09271bf18da6c873375768fb29e216
ashish5514 <zankatashish680@gmail.com>
2023-08-11 11:52:33 +0530

    work

---
+++ java practice/02/String.js
@@ -0,0 +1,25 @@
+//    string indexing ... 
+
+let firstname ="ashishzxcvbnm";
+
+// a s h i s h
+// 0 1 2 3 4 5
+
+//  console.log(firstname[0]); 
+
+// length of string 
+//firstname.length
+// space use to length add in log..
+// space use in length
+
+console.log(firstname.length);
+
+
+// last index seen to use 
+// length - 1
+
+console.log(firstname[firstname.length-1]);
+
+
+ 
+ 
---
+++ java practice/02/string_meathods.js
@@ -0,0 +1,32 @@
+ //trim ()
+ // touppercase ()
+ // tolowercase()
+ // slice
+
+ // srting in space dlet use to trim methods
+
+ let firstname = "ashish";
+
+
+//  console.log(firstname.length);
+ // remove space 
+// firstname = firstname.trim();
+// console.log(firstname);
+//  console.log(firstname.length);
+
+
+// touppercase  variable vlue changed to capital text
+// firstname = firstname.toUpperCase();
+// console.log(firstname);
+
+// tolowercase  variable value changed to samll text
+// firstname = firstname.toLowerCase();
+// console.log(firstname);
+
+
+// slice methode
+
+// start index to end index
+// let newstring = firstname.slice(0,4);
+//  console.log(newstring);
+
